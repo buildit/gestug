d980cd53a4555430887778428c37e81bdf440c21
Peter Monks <walter.monks@wipro.comx>
2017-04-06 12:59:40 -0400

    feh

--- scripts/brandai.js
+++ scripts/brandai.js
@@ -1,14 +1,18 @@
 module.exports = function(robot) {
 
-  robot.hear(/A color was added to the (.*) design library/, function(res) {
+  var kickoffBrandaiUpdate = function(res) {
+    res.send('The design library I detected was ' + msg.match[1]);
     if (res.message.user.name == 'Brand.ai') {
       res.send('I heard you, Brand.ai!');
     }
     else {
       res.send('You can\'t trick me!');
     }
+  }
 
-  });
+  robot.hear(/The color .* was changed in the (.*) design library/, kickoffBrandaiUpdate);
+  robot.hear(/A color was added to the (.*) design library/, kickoffBrandaiUpdate);
+  robot.hear(/The font family .* was added to the (.*) design library/, kickoffBrandaiUpdate);
   robot.hear(/yo/, function(msg) { msg.send('Yo') });
 
   robot.hear(/(.*)/, function(msg) {
@@ -19,3 +23,5 @@ module.exports = function(robot) {
 
 // Brand.ai
 // A color was added to the Primary Brand design library by walter.monks@wipro.com.\nUntitled Color:rgb(231, 67, 213)
+// The font family Helvetica was added to the Primary Brand design library
+// The color “Brand 3” was changed in the Primary Brand design library
