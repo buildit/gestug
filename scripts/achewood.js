c756b111867e0e1ca54d79a7a57ba5bc0cdcb003
Peter Monks <walter.monks@wipro.comx>
2017-04-06 15:13:48 -0400

    Added an achewood plugin, because why not?

---
+++ scripts/achewood.js
@@ -0,0 +1,65 @@
+// # Description
+// #   Philippe is standing on it.
+// #
+// # Dependencies:
+// #  'htmlparser': '1.7.6'
+// #  'soupselect': '0.2.0'
+// #
+// # Configuration:
+// #   None
+// #
+// # Commands:
+// #   hubot achewood - A random Achewood comic
+// #   hubot achewood current - The most recent Achewood comic
+// #   hubot achewood <date> - Achewood comic from <date> - mm/dd/yyyy format
+// #   hubot achewood <keyword> - Achewood comic for keyword
+// #   hubot saddest thing - The saddest thing, according to Lie Bot
+// #
+// # Author:
+// #   1000hz
+
+var htmlparser = require('htmlparser');
+var Select = require('soupselect').select;
+
+module.exports = function(robot) {
+  var withDate = function(date) { return 'http://achewood.com/index.php?date=' + date }
+
+  var fetchAchewood = function(msg, url) {
+    console.log(url);
+    msg.http(url).get()(function(err, res, body) {
+      var handler = new htmlparser.DefaultHandler();
+      var parser = new htmlparser.Parser(handler);
+
+      parser.parseComplete(body);
+      var img = Select(handler.dom, 'img.comic');
+      var comic = img[0].attribs;
+
+      msg.send('http://achewood.com' + comic.src + '#.png');
+      msg.send(comic.title);
+    });
+  };
+
+  var dateRegex = /achewood\s?((?:0[1-9]|1[0-2]).?(?:0[1-9]|[1-2][0-9]|3[0-1]).?(?:20\d{2})$|.*)?/i;
+  robot.respond(dateRegex, function(msg) {
+    var arg = msg.match[1];
+    if (!arg) {
+      msg.http('http://www.ohnorobot.com/random.pl?comic=636')
+        .get()(function(err, res, body) { fetchAchewood(msg, res.headers['location'])});
+    }
+    else if (arg === 'current') {
+      fetchAchewood(msg, 'http://achewood.com');
+    }
+    else if (arg.match(/\d{2}.?\d{2}.?\d{4}/)) {
+      fetchAchewood(msg, withDate(arg.replace(/\D/g, '')));
+    }
+    else {
+      msg.http('http://www.ohnorobot.com/index.pl?comic=636&lucky=1&s=' + arg)
+        .get()(function(err, res, body) { fetchAchewood(msg, res.headers['location'])});
+    }
+  });
+
+  robot.respond(/.*saddest thing\?*/i, function(msg) {
+    var saddest = msg.random(['06022003', '11052001', '09052006', '07302007', '12102001']);
+    fetchAchewood(msg, withDate(saddest));
+  });
+};
--- scripts/brandai.js
+++ scripts/brandai.js
@@ -2,7 +2,7 @@ module.exports = function(robot) {
 
   var kickoffBrandaiUpdate = function(res) {
     console.log('in this');
-    res.send('The design library I detected was ' + msg.match[1]);
+    res.send('The design library I detected was ' + res.match[1]);
     if (res.message.user.name == 'Brand.ai') {
       res.send('I heard you, Brand.ai!');
     }
@@ -11,18 +11,24 @@ module.exports = function(robot) {
     }
   }
 
-  robot.hear(/The color .? was changed in the (.?) design library/, kickoffBrandaiUpdate);
-  robot.hear(/A color was added to the (.?) design library/, kickoffBrandaiUpdate);
-  robot.hear(/The font family .? was added to the (.?) design library/, kickoffBrandaiUpdate);
-  robot.hear(/yo/, function(msg) { msg.send('Yo') });
+  var kickoffManualBrandaiUpdate = function(res) {
+    if (res.message.user.name !== 'Brand.ai') {
+      res.send('Kicking off!');
+    }
+  }
 
-  robot.hear(/(.*)/, function(msg) {
-    console.log(msg.match[1]);
-    console.log(msg);
-  });
+  robot.hear(/The color .* was changed in the (.*) design library/, kickoffBrandaiUpdate);
+  robot.hear(/A color was added to the (.*) design library/, kickoffBrandaiUpdate);
+  robot.hear(/The font family .* was added to the (.*) design library/, kickoffBrandaiUpdate);
+  robot.hear(/The (logo|icon|image) .* was added to the (.*) design library/, kickoffBrandaiUpdate);
+
+  robot.hear(/kickoff brandai/, kickoffManualBrandaiUpdate)
 }
 
 // Brand.ai
 // A color was added to the Primary Brand design library by walter.monks@wipro.com.\nUntitled Color:rgb(231, 67, 213)
 // The font family Helvetica was added to the Primary Brand design library
 // The color “Brand 3” was changed in the Primary Brand design library
+// The icon "rocket.svg" was added to the Primary Brand design library
+// The image "sea_view.jpg" was added to the Primary Brand design library
+// The logo "icon-logo.svg" was added to the Primary Brand design library
