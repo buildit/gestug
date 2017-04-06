157c2792ec83842a21572394ff16c4501412ff4f
Peter Monks <walter.monks@wipro.comx>
2017-04-06 11:28:36 -0400

    first brandai actual integration

--- scripts/brandai.js
+++ scripts/brandai.js
@@ -1,4 +1,14 @@
 module.exports = function(robot) {
+
+  robot.hear(/A color was added to the (.*) design library/, function(res) {
+    if (res.message.user.name == 'Brand.ai') {
+      response.send('I heard you, Brand.ai!');
+    }
+    else {
+      response.send('You can\'t trick me!');
+    }
+
+  });
   robot.hear(/yo/, function(msg) { msg.send('Yo') });
 
   robot.hear(/(.*)/, function(msg) {
@@ -6,3 +16,6 @@ module.exports = function(robot) {
     console.log(msg);
   });
 }
+
+// Brand.ai
+// A color was added to the Primary Brand design library by walter.monks@wipro.com.\nUntitled Color:rgb(231, 67, 213)
