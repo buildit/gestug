251bd037d9efdd940ebd0efe1f93e7708ef3f8e8
Peter Monks <walter.monks@wipro.comx>
2017-04-13 10:07:56 -0400

    added color change notification to brandai listener

--- src/es6/brandai.js
+++ src/es6/brandai.js
@@ -26,6 +26,7 @@ const kickoffManualBrandaiUpdate = res => {
 const setupRobot = robot => {
   robot.hear(/The color .* was changed in the (.*) design library/, kickoffBrandaiUpdate);
   robot.hear(/A color was added to the (.*) design library/, kickoffBrandaiUpdate);
+  robot.hear(/A color was changed in the (.*) design library/, kickoffBrandaiUpdate);
   robot.hear(/The font family .* was added to the (.*) design library/, kickoffBrandaiUpdate);
   robot.hear(/The (logo|icon|image) .* was added to the (.*) design library/, kickoffBrandaiUpdate);
 
